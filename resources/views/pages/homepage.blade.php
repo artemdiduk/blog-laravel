5471fa206cc541701e6dda487e6459fe0e07f6f8
artemdiduk <artem_didyk@ukr.net>
2023-04-27 20:28:03 +0300

    added docker file

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -15,7 +15,7 @@
         <div class="col-md-12 border-bottom">
         @foreach ($groups as $group)
             <div class="row">
-                <div class="col-4">
+                <div class="col-12">
                     <a href="{{route("group", $group)}}">{{$group->name}}</a>
                 </div>
             </div>
